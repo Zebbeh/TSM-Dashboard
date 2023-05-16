78110d0fedb0f44e307d166f95456f8b162ed874
Sebastian <haggblca@arcada.fi>
2023-05-16 17:32:54 +0300

    added graphs (bar and pie)

--- dahsboard.py
+++ dahsboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import requests
 
 def get_access_token(api_key):
@@ -36,7 +37,8 @@ def settings_page():
     st.title("Settings")
     st.header("API Key")
 
-    # Get the API key from the user
+    # Get the
+    #  API key from the user
     api_key = st.text_input("Enter your API key")
 
     if st.button("Save"):
@@ -48,7 +50,7 @@ def settings_page():
             st.session_state.access_token = access_token
 
 
-def main_page():
+def server_page():
     
 
     if "access_token" not in st.session_state:
@@ -96,8 +98,7 @@ def main_page():
             if selected_realm_id:
                 url = f"https://realm-api.tradeskillmaster.com/realms/{selected_realm_id}"
                 response = requests.get(url, headers={"Authorization": f"Bearer {access_token}"})
-                st.title(f"Information for {server_options[selected_realm_index]}")          
-                st.write(response.json())
+                st.title(f"{server_options[selected_realm_index]} Server information")          
                 ahs = response.json().get("auctionHouses", [])
                 ah_options = [ah["type"] for ah in ahs]
                 ah_id = [ah["auctionHouseId"] for ah in ahs]
@@ -105,9 +106,89 @@ def main_page():
                 selected_ah_index = st.sidebar.selectbox("Select Faction", range(len(ah_options)), format_func=lambda i: ah_options[i])
                 selected_ah_id = ah_id[selected_ah_index] if ahs else None
                 
-                url = f"https://pricing-api.tradeskillmaster.com/ah/{selected_ah_id}"
-                response = requests.get(url, headers={"Authorization": f"Bearer {access_token}"})
-                st.write(response.json())
+                #url = f"https://pricing-api.tradeskillmaster.com/ah/{selected_ah_id}"
+                #response = requests.get(url, headers={"Authorization": f"Bearer {access_token}"})
+                #items = response.json()
+
+
+
+                # "Alexstraszite": 192852, "Ysemerald": 192859, "Nozdorite": 192866, "Malygite": 192856, "Neltharite": 192862
+                herb_list = [
+
+                ]
+                ore_list = [
+
+                ]
+                cloth_list = [
+
+                ]
+                gems = [
+                    {"id": 192852, "name": "Alexstraszite", "minBuyout": None, "quantity": None, "marketValue": None, "historical": None, "numAuctions": None},
+                    {"id": 192859, "name": "Ysemerald", "minBuyout": None, "quantity": None, "marketValue": None, "historical": None, "numAuctions": None},
+                    {"id": 192866, "name": "Nozdorite", "minBuyout": None, "quantity": None, "marketValue": None, "historical": None, "numAuctions": None},
+                    {"id": 192856, "name": "Malygite", "minBuyout": None, "quantity": None, "marketValue": None, "historical": None, "numAuctions": None},
+                    {"id": 192862, "name": "Neltharite", "minBuyout": None, "quantity": None, "marketValue": None, "historical": None, "numAuctions": None}
+                    ]
+                for gem in gems:
+                    gem_id = gem["id"]
+                    response = requests.get(f"https://pricing-api.tradeskillmaster.com/ah/{selected_ah_id}/item/{gem_id}", headers={"Authorization": f"Bearer {access_token}"})
+                    if response.status_code == 200:
+                        gem_details = response.json()
+
+                        gem["minBuyout"] = gem_details["minBuyout"]
+                        gem["quantity"] = gem_details["quantity"]
+                        gem["marketValue"] = gem_details["marketValue"]
+                        gem["historical"] = gem_details["historical"]
+                        gem["numAuctions"] = gem_details["numAuctions"]
+                    else:
+                        print(f"Error in gem details request for gem ID {gem_id}")   
+
+
+
+
+                # DataFrame for gems        
+                df = pd.DataFrame(gems)
+                pd.options.display.float_format = '{:,.0f}'.format
+                df["id"] = df["id"].astype(int)
+                df.set_index("id", inplace=True)
+                
+                # Create a bar chart
+                
+                # Checkbox to deside if you want to see the gem information or not
+                show_gems = st.checkbox("Show Gems", value=True)
+                #show_ores = st.checkbox("Show Ores", value=True)
+
+                chart_data = df[['name','minBuyout', 'marketValue']].set_index('name')
+                fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+                if show_gems:
+                    chart_data.plot(kind='bar', ax=ax1)
+                    ax1.set_xlabel('Gem Name')
+                    ax1.set_ylabel('Value')
+                    ax1.set_title('Gem Buyout and Market Values')
+                    ax1.set_xticklabels(ax1.get_xticklabels(), rotation=45)
+                pie_chart_data = df.set_index('name')['quantity']
+
+                ax2.pie(pie_chart_data, labels=pie_chart_data.index, autopct='%1.1f%%', startangle=90)
+                ax2.set_title('Gem Quantity in Auction House')
+
+                plt.subplots_adjust(wspace=0.4)
+                
+
+
+                st.pyplot(fig)
+                    
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
@@ -118,7 +199,7 @@ def main():
     if selected_page == "Settings":
         settings_page()
     elif selected_page == "Server":
-        main_page()
+        server_page()
 
 if __name__ == "__main__":
     main()
